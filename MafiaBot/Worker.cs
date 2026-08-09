388bc289098c37bad5fb1405a3d4198d84b64607
chebyrach <chebyrach@gmail.com>
2026-08-10 00:13:19 +0300

    added logging

--- MafiaBot/Worker.cs
+++ MafiaBot/Worker.cs
@@ -6,7 +6,6 @@ using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public class Worker : BackgroundService
 {
@@ -27,12 +26,12 @@ public class Worker : BackgroundService
         bot.OnUpdate += OnUpdate;
         async Task OnError(Exception exception, HandleErrorSource source)
         {
-            Console.WriteLine(exception); // just dump the exception to the console
+            Console.WriteLine(exception);
         }
 
-        // method that handle messages received by the bot:
         async Task OnMessage(Message msg, UpdateType type)
         {
+            _logger.LogInformation($"Message received: {msg.From}: {msg.Text}");
             if (msg.Text == "/start")
             {
                 await bot.SendMessage(msg.Chat, "Welcome! Pick one direction",
@@ -40,23 +39,13 @@ public class Worker : BackgroundService
             }
         }
 
-        // method that handle other types of updates received by the bot:
         async Task OnUpdate(Update update)
         {
-            if (update is { CallbackQuery: { } query }) // non-null CallbackQuery
+            if (update is { CallbackQuery: { } query })
             {
                 await bot.AnswerCallbackQuery(query.Id, $"You picked {query.Data}");
                 await bot.SendMessage(query.Message!.Chat, $"User {query.From} clicked on {query.Data}");
             }
-        }
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-
-            await Task.Delay(1000, stoppingToken);
         }
     }
 }
