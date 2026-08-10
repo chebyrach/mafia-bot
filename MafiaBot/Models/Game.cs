6b90b3fa65514d74df286ca49b17ea0cf7492b67
chebyrach <chebyrach@gmail.com>
2026-08-10 19:18:54 +0300

    added game logic

--- MafiaBot/Models/Game.cs
+++ MafiaBot/Models/Game.cs
@@ -3,6 +3,8 @@ namespace MafiaBot.Models;
 
 public class Game
 {
+    private Player mafiaTarget;
+    private Player doctorTarget;
     public List<Player> Players { get; set; }
 
     public Game(List<string> players)
@@ -12,6 +14,7 @@ public class Game
         foreach (var player in players)
         {
             Player newPlayer = new Player(player, Roles.Civilian);
+            Players.Add(newPlayer);
         }
         GiveRoles();
     }
@@ -58,5 +61,60 @@ public class Game
             }
         }
         Players.Shuffle();
+    }
+
+    public void MafiaWalks(string targetId)
+    {
+        var targetObj = Players.First(x => x.Tag == targetId);
+        if(targetObj != null) throw new NullReferenceException();
+        mafiaTarget = targetObj;
+    }
+    
+    public bool isDetectiveAlive()
+    {
+        var doctorObj = Players.First(x => x.CheckForDetective());
+        return doctorObj.IsAlive;
+    }
+
+    public bool DetectiveWalks(string targetId)
+    {
+        var targetObj = Players.First(x => x.Tag == targetId);
+        if(targetObj != null) throw new NullReferenceException();
+        return targetObj.CheckForMafia();
+    }
+    
+    public bool isDoctorAlive()
+    {
+        var doctorObj = Players.First(x => x.CheckForDoctor());
+        return doctorObj.IsAlive;
+    }
+    
+    public void DoctorWalks(string targetId)
+    {
+        var targetObj = Players.First(x => x.Tag == targetId);
+        if(targetObj != null) throw new NullReferenceException();
+        doctorTarget = targetObj;
+    }
+
+    public string? CheckRoundResults()
+    {
+        if (mafiaTarget.Equals(doctorTarget))
+        {
+            return null;
+        }
+        Players.First(x => x.Equals(mafiaTarget)).Kill();
+        return mafiaTarget.Tag;
+    }
+
+    public bool CheckForCivilianWin()
+    {
+        return Players.First(x => x.CheckForMafia() && x.CheckforAlive() ) == null;
+    }
+    
+    public bool CheckForMafiaWin()
+    {
+        int civilians =  Players.Where(x => !x.CheckForMafia()).Count();
+        int mafias =  Players.Where(x => x.CheckForMafia()).Count();
+        return civilians >= mafias;
     }
 }
--- MafiaBot/Models/Player.cs
+++ MafiaBot/Models/Player.cs
@@ -13,9 +13,38 @@ public class Player
         Tag = tag;
         Role = role;
     }
+
+    public bool CheckForMafia()
+    {
+        if (this.Role == Roles.Mafia) return true;
+        return false;
+    }
+    
+    public bool CheckForDetective()
+    {
+        if (this.Role == Roles.Detective) return true;
+        return false;
+    }
+    
+    public bool CheckForDoctor()
+    {
+        if (this.Role == Roles.Doctor) return true;
+        return false;
+    }
     
     public void Kill()
     {
         IsAlive = false;
+    }
+
+    public bool CheckforAlive()
+    {
+        if (IsAlive) return true;
+        return false;
+    }
+    
+    public bool Equals(Player? obj)
+    {
+        return this.Tag.Equals(obj.Tag);
     }
 }
